8beedb6abcb7cfffb9300c80c467134aa75a6ee4
LuccaMauroMolina <roxanalucca79@gmail.com>
2024-01-24 15:55:07 -0300

    actualizado

--- src/Asynmock.js
+++ src/Asynmock.js
@@ -1,8 +1,8 @@
 export const Productos = [
-    {nombre: "Brownie", precio: 800, img: "./img/brownie.png", descripcion: "este es uno de los mejores brownies" },
-    {nombre: "Brownie full", precio: 2500,img:"./img/maicena.png", descripcion: "este es uno de los mejores brownies" },
-    {nombre: "Brownie stela", precio: 700, img:"./img/brownie.png", descripcion: "este es uno de los mejores brownies" },
-    {nombre: "alfajores", precio: 1200,img:"./img/maicena.png", descripcion: "este es uno de los mejores brownies" },
+    {nombre: "Brownie", precio: 800, img: "./imgReact/brownie.png", descripcion: "este es uno de los mejores brownies" },
+    {nombre: "Brownie full", precio: 2500,img:"./imgReact/maicena.png", descripcion: "este es uno de los mejores brownies" },
+    {nombre: "Brownie stela", precio: 700, img:"./imgReact/brownie.png", descripcion: "este es uno de los mejores brownies" },
+    {nombre: "alfajores", precio: 1200,img:"./imgReact/maicena.png", descripcion: "este es uno de los mejores brownies" },
 ]
 
 
